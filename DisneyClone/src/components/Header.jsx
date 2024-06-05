38039a7bf1c1ff52c3b00a10aa14e0de3dec1734
Nelson Mungai <nelson.irungu@students.jkuat.ac.ke>
2024-06-05 11:15:27 +0300

    made the Navbar responsive" for both medium size screen devices, and small screen devices

--- DisneyClone/src/components/Header.jsx
+++ DisneyClone/src/components/Header.jsx
@@ -1,53 +1,71 @@
-import logo from "./..//assets/images/DisneyLogo.png"
-import profile from "./../assets/images/profile.png"
-import {
-    HiHome,
+import logo from "../assets/images/DisneyLogo.png"
+import profile from "../assets/images/profile.png"
+import { HiHome,
     HiMagnifyingGlass,
     HiStar,
     HiPlayCircle,
-    HiTv,
-    HiPlus
-} from "react-icons/hi2"
-const menu=[
-    {
-        name:"HOME",
-        icon:HiHome
-    },
-    {
-        name:"SEARCH",
-        icon:HiMagnifyingGlass
-    },
-    {
-        name:"WATCHLIST",
-        icon:HiPlus
-    },
-    {
-        name:"ORGINALS",
-        icon:HiStar
-    },
-    {
-        name:"MOVIES",
-        icon:HiPlayCircle
-    },
-    {
-        name:"SERIES",
-        icon:HiTv
-    }
+    HiTv
+ } from "react-icons/hi2";
+ import { HiPlus,HiDotsVertical } from "react-icons/hi";
+import HeaderItem from "./HeaderItem";
+import { useState } from "react";
+function Header() {
+    const [toogle,setToggle]=useState(false)
+    const menu=[
+        {
+            name:"HOME",
+            icon:HiHome
+        },
+        {
+            name:"SEARCH",
+            icon:HiMagnifyingGlass
+        },
+        {
+            name:"WATCHLIST",
+            icon:HiPlus
+        },
+        {
+            name:"ORIGINALS",
+            icon:HiStar
+        },
+        {
+            name:"MOVIES",
+            icon:HiPlayCircle
+        },
+        {
+            name:'TV SHOWS',
+            icon:HiTv
+        }
 
-]
-import HeaderItem from "./HeaderItem"
-function Header(){
-    
-    return (
-        <div className="flex items-center justify-between p-5">
-        <div className="flex items-center gap-3">
-        <img src={logo} alt="" className="w-[80px]md:w-[115px] object-cover" />
+    ]
+  return (
+    <div className="flex items-center justify-between p-5">
+        <div className="flex items-center gap-5">
+        <img src={logo} className="w-[80px] object-cover md:w-[120px]" alt=""/>
+        <div className="hidden md:flex gap-8">
         {menu.map((item,index)=>(
             <HeaderItem key={index} name={item.name} Icon={item.icon}/>
         ))}
         </div>
-        <img className="w-[40px] rounded-full" src={profile} alt="" />
+        {/* responsive menu */}
+        <div className="flex md:hidden gap-8">
+        {menu.map((item,index)=>index<3&&(
+            <HeaderItem key={index} name={""} Icon={item.icon}/>
+        ))}
         </div>
-    )
+        {/* 3 vertical dots */}
+        <div className="md:hidden" onClick={()=>setToggle(!toogle)}>
+            <HeaderItem name={""} Icon={HiDotsVertical}/>
+            {toogle?<div className="absolute mt-3 bg-[#181a34] border-[1px] border-gray-700 p-3 px-5 py-4">
+            {menu.map((item,index)=>index>=3&&(
+            <HeaderItem key={index} name={item.name} Icon={item.icon}/>
+        ))}
+            </div>:null}
+        </div>
+        </div>
+        <img src={profile} alt="" className="w-[40px] rounded-full" />
+    </div>
+  )
 }
-export default Header
+
+export default Header;
